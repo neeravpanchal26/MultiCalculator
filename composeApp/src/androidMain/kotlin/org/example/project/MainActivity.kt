a21a3cf5bbdebe74b84dfd172cd474ce65032b68
Neerav Panchal <neeravpanchal26@gmail.com>
2024-06-09 09:16:11 -0400

    [06-06-24]
    Edit the "CalcEqualsButton" to take a parameter of MutableState<String>, name this parameter "display".

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -4,8 +4,13 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +44,7 @@ fun CalcOperationButton() {
 }
 
 @Composable
-fun CalcEqualsButton() {
+fun CalcEqualsButton(display: MutableState<String>) {
 }
 
 @Composable
